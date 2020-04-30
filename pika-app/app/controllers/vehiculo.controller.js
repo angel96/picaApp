9d87d673afe0634ddc6a7c2ed697a14c72941639
Ezequiel Portillo Jurado <ezequiel6a@gmail.com>
2020-04-30 18:03:25 +0200

    Crud completo de vehiculo

--- pika-app/app/controllers/vehiculo.controller.js
+++ pika-app/app/controllers/vehiculo.controller.js
@@ -6,7 +6,7 @@ exports.create = (req, res) => {
     //Comprobar respuesta valida
     if(!req.body.matricula){
         return res.status(400).send({
-            message: "El nombre del vehiculo no puede estar vacío"
+            message: "La matrícula del vehículo no puede estar vacío"
         });
     }
 
@@ -32,19 +32,97 @@ exports.create = (req, res) => {
 // Retrieve and return all vehiculos from the database.
 exports.findAll = (req, res) => {
 
+    //Listar todos los vehiculos
+    Vehiculo.find().then(vehiculos => {
+        res.send(vehiculos);
+    }).catch(error => {
+        res.status(500).send({
+            message: error.message || "Un error ha ocurrido al recibir la lista."
+        })
+    })
+
 };
 
 // Find a single vehiculo with a vehiculoId
 exports.findOne = (req, res) => {
+
+    //Encuentra el vehiculo por el id
+    Vehiculo.findById(req.params.vehiculoId).then(vehiculo => {
+        if(!vehiculo) {
+            //Si no te llega devuelve un 404 de vehiculo no encontrado
+            return res.status(404).send({
+                message: "Vehículo con id " + req.params.vehiculoId + " no encontrado."
+            })
+        }
+        res.send(vehiculo);
+    }).catch(error => {
+        if(error.kind === 'ObjectId') {
+            res.status(404).send({
+                message: "Vehículo con id " + req.params.vehiculoId + " no encontrado."
+            })
+        }
+        //Si no le llega el id
+        return res.status(500).send({
+            message: "Error recibiendo el vehículo con la id " + req.params.vehiculoId
+        })
+    })
 
 };
 
 // Update a vehiculo identified by the vehiculoId in the request
 exports.update = (req, res) => {
 
+    //Comprobar respuesta valida
+    if(!req.body.matricula){
+        return res.status(400).send({
+            message: "la matrícula del vehiculo no puede estar vacío"
+        });
+    }
+
+    //Actualiza el vehiculo
+    Vehiculo.findByIdAndUpdate(req.params.vehiculoId, {
+        propietario: req.body.propietario || "Sin propietario",
+        tipo: req.body.tipo || "NO TIPO",
+        cargaMaxima: req.body.cargaMaxima || "Sin carga máxima",
+        matricula: req.body.matricula
+    }, {new: true}).then(vehiculo => {
+        if(!vehiculo) {
+            return res.status(404).send({
+                message: "vehículo no encontrado con el id " + req.params.vehiculoId
+            });
+        }
+        res.send(vehiculo);
+    }).catch(error => {
+        if(error.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "vehículo no encontrado con el id " + req.params.vehiculoId
+            });                
+        }
+        return res.status(500).send({
+            message: "vehículo no encontrado con el id " + req.params.vehiculoId
+        });
+    });
 };
 
 // Delete a vehiculo with the specified vehiculoId in the request
 exports.delete = (req, res) => {
+    
+    Vehiculo.findByIdAndRemove(req.params.vehiculoId).then(vehiculo => {
+        if(!vehiculo) {
+            return res.status(404).send({
+                message: "vehículo no encontrado con el id " + req.params.vehiculoId
+            });
+        }
+        res.send({message: "Vehículo borrado correctamente!"});
+    }).catch(error => {
+        if(error.kind === 'ObjectId' || error.name === 'NotFound') {
+            return res.status(404).send({
+                message: "vehículo no encontrado con el id " + req.params.vehiculoId
+            });                
+        }
+        return res.status(500).send({
+            message: "vehículo no encontrado con el id " + req.params.vehiculoId
+        });
+    });
 
 };
