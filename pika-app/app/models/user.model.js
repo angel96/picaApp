8dac8359f215dc3b5534af57c2346ca295c9c253
Ángel Delgado Luna <angeldelgadoluna@gmail.com>
2020-05-10 05:32:26 +0200

    Herencia Implementada. Falta actualización de métodos
    Actualización de métodos de creación y edición y revisar que el usuario neutro funcione adecuadamente.

--- pika-app/app/models/user.model.js
+++ pika-app/app/models/user.model.js
@@ -1,20 +1,39 @@
+//https://verticalaxisbd.com/blog/inheritance-mongodb-mongoose/
+
 const mongoose = require('mongoose');
 var UserAccount = require('../models/userAccount.model.js');
+var util = require('util');
+var Schema = mongoose.Schema;
 mongoose.model('UserAccount');
 require('./userAccount.model');
-//Crea el esquema de la clase
-const userSchema = mongoose.Schema({
-    userAccount: { 
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'UserAccount'
-     },
-    name: String,
-    surnames: String,
-    location: String
-//Añade automaticamente createdAt u UpdatedAt
-}, {
-    timestamps: true,
-    autoCreate: true
-});
 
-module.exports = mongoose.model('User', userSchema);
+function BaseSchema(){
+    Schema.apply(this,{
+        timestamps: true,
+        autoCreate: true
+    });
+
+    this.add({
+        userAccount: { 
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'UserAccount'
+         },
+        name: String,
+        surnames: String,
+        location: String
+    });
+}
+
+util.inherits(BaseSchema, Schema);
+
+var UserSchema = new BaseSchema();
+
+UserSchema.virtual('type').get(function () { return this.__t; });
+
+var User = mongoose.model('User', UserSchema);
+
+var usuarioVehiculo = new BaseSchema({creditCard:{type:String}});
+User.discriminator('UsuarioVehiculo', usuarioVehiculo);
+
+var usuarioLocal = new BaseSchema({numeroLocal:{type:String}});
+User.discriminator('UsuarioLocal', usuarioLocal);
